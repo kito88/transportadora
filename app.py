164f54bdb3f43fe2759ef371ef51622322e3fc61
Francisco Gonçalves Pereira <109889534+kito88@users.noreply.github.com>
2025-10-10 09:32:44 -0300

    Atualizar o app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-6import requests
+import requests
 import io
 from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify, send_file
 from reportlab.pdfgen import canvas
